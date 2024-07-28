b3b051a3d6dfd5da6f8bb2c6114d7d828226fe5b
ItsKruistz <lekruistz@gmail.com>
2024-07-28 18:31:33 +0700

    First

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import "beercss";
+</script>
+
+<main>
+  <header>
+    <nav>
+      <button class="circle transparent">
+        <i>arrow_back</i>
+      </button>
+      <h5 class="max">Title</h5>
+      <button class="circle transparent">
+        <i>attach_file</i>
+      </button>
+      <button class="circle transparent">
+        <i>today</i>
+      </button>
+      <button class="circle transparent">
+        <i>more_vert</i>
+      </button>
+    </nav>
+  </header>
+</main>
