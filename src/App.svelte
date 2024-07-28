9ae625bee8b947c27cd4512fefff942cca0a88fe
ItsKruistz <lekruistz@gmail.com>
2024-07-28 20:31:24 +0700

    Update

--- src/App.svelte
+++ src/App.svelte
@@ -5,17 +5,17 @@
 <main>
   <header>
     <nav>
-      <button class="circle transparent">
+      <button class="circle small transparent">
         <i>arrow_back</i>
       </button>
       <h5 class="max">Title</h5>
-      <button class="circle transparent">
+      <button class="circle small transparent">
         <i>attach_file</i>
       </button>
-      <button class="circle transparent">
+      <button class="circle small transparent">
         <i>today</i>
       </button>
-      <button class="circle transparent">
+      <button class="circle small transparent">
         <i>more_vert</i>
       </button>
     </nav>
